e83da82166b5e4fd334162d890e6ba0455a2e2ed
49513 - Diogo Rodrigues <a49513@alunos.isel.pt>
2023-10-10 17:19:07 +0100

    Relational model done and created the schema of that model. Added the schema that adds data to the tables.

--- code/jvm/src/sql/create-schema.sql
+++ code/jvm/src/sql/create-schema.sql
@@ -1,25 +1,45 @@
 create schema dbo;
 
-create table dbo.Users(
-    id int generated always as identity primary key,
-    username VARCHAR(64) unique not null,
-    password_validation VARCHAR(256) not null
+create table dbo.Lobby(
+    lobby_id int generated always as identity primary key,
+    joined_at timestamp not null
 );
 
-create table dbo.Tokens(
-    token_validation VARCHAR(256) primary key,
-    user_id int references dbo.Users(id),
-    created_at bigint not null,
-    last_used_at bigint not null
-);
-
-create table dbo.Games(
-    id UUID not null,
-    state VARCHAR(64) not null,
+create table dbo.Game(
+    game_id serial primary key ,
+    state VARCHAR(64) check (state in ('WAITING FOR PLAYERS', 'PLAYING', 'FINISHED')) not null,
     board jsonb not null,
-    created int not null,
-    updated int not null,
-    deadline int,
-    player_x int references dbo.Users(id),
-    player_o int references dbo.Users(id)
+    size int not null,
+    variant varchar(64) check (variant in ('FREESTYLE', 'RENJU', 'CARO', 'OMOK', 'NINUKI-RENJU', 'PENTE')) not null,
+    opening_rule varchar(64) check (opening_rule in ('PRO', 'LONG-PRO', 'SWAP', 'SWAP2')) not null
 );
+
+create table dbo.User(
+    user_id int generated always as identity primary key,
+    username VARCHAR(64) not null,
+    email VARCHAR(64) unique not null,
+    password_validation VARCHAR(256) not null,
+    lobby int references dbo.Lobby(lobby_id),
+    game int references dbo.Game(game_id)
+);
+
+create table dbo.Token(
+    token_validation VARCHAR(256) primary key,
+    created_at bigint not null,
+    last_used_at bigint not null,
+    user_id int references dbo.User(user_id)
+);
+
+create table dbo.Statistic(
+    user_id int primary key,
+    rank int not null,
+    points int not null,
+    games_played int not null,
+    games_won int not null,
+    foreign key (user_id) references dbo.User(user_id)
+);
+
+
+
+
+
---
+++ code/jvm/src/sql/insert-test-data.sql
@@ -0,0 +1,36 @@
+-- Insert data into dbo.Lobby
+INSERT INTO dbo.Lobby (joined_at)
+VALUES ('2023-10-10 12:00:00'),
+       ('2023-10-11 14:30:00'),
+       ('2023-10-12 10:15:00');
+
+-- Insert data into dbo.Game
+INSERT INTO dbo.Game (state, board, size, variant, opening_rule)
+VALUES ('WAITING FOR PLAYERS',
+        '{"data": {"moves": [{"x": 1,"y": ''f'',"color": "BLACK"},{"x": 2,"y": ''e'',"color": "WHITE"}],"turn": "BLACK"}}',
+        15, 'FREESTYLE', 'PRO'),
+       ('PLAYING',
+        '{"data": {"moves": [{"x": 1,"y": ''f'',"color": "WHITE"},{"x": 2,"y": ''e'',"color": "BLACK"}],"turn": "WHITE"}}',
+        15, 'RENJU', 'LONG-PRO'),
+       ('FINISHED',
+        '{"data": {"moves": [{"x": 1,"y": ''f'',"color": "BLACK"},{"x": 2,"y": ''e'',"color": "WHITE"}],"turn": "BLACK"}}',
+        19, 'CARO', 'SWAP2');
+
+-- Insert data into dbo.User
+INSERT INTO dbo.User (username, email, password_validation, lobby, game)
+VALUES ('user1', 'user1@example.com', 'password_hash_1', 1, 1),
+       ('user2', 'user2@example.com', 'password_hash_2', 1, 1),
+       ('user3', 'user3@example.com', 'password_hash_3', 2, 3);
+
+-- Insert data into dbo.Token
+INSERT INTO dbo.Token (token_validation, created_at, last_used_at, user_id)
+VALUES ('token_1', 1633824000, 1633904000, 1),
+       ('token_2', 1633910400, 1634004000, 2),
+       ('token_3', 1634090400, 1634184000, 3);
+
+-- Insert data into dbo.Statistic
+INSERT INTO dbo.Statistic (user_id, rank, points, games_played, games_won)
+VALUES (1, 1, 100, 10, 5),
+       (2, 2, 80, 8, 3),
+       (3, 3, 60, 6, 2);
+
