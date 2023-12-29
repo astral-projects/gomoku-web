ba0240ca851a92a586f5166f30fb4a2dc3dd5b22
Francisco Engenheiro <a49428@alunos.isel.pt>
2023-12-29 17:37:02 +0000

    Minor changes in README.md.

--- code/js/src/App.tsx
+++ code/js/src/App.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
-import { GomokuContainer } from './pages/GomokuContainer';
-import { Home } from './pages/home/Home';
-import { Login } from './pages/login/Login';
-import { Me } from './pages/me/Me';
-import { Error } from './pages/error/Error';
-import { Register } from './pages/register/Register';
-import { FindGame } from './pages/findGame/FindGame';
-import { Game } from './pages/game/Game';
-import { About } from './pages/about/About';
-import { Logout } from './pages/logout/Logout';
-import { UserStats } from './pages/userstats/UserStats';
-import { RequireAuthn } from './pages/AuthContainer';
-import { NotFound } from './pages/notFound/NotFound';
-import { Lobby } from './pages/lobby/Lobby';
-import { Rankings } from './pages/Rankings/Rankings';
+import {createBrowserRouter, Outlet, RouterProvider} from 'react-router-dom';
+import {GomokuContainer} from './pages/GomokuContainer';
+import {Home} from './pages/home/Home';
+import {Login} from './pages/login/Login';
+import {Me} from './pages/me/Me';
+import {Error} from './pages/error/Error';
+import {Register} from './pages/register/Register';
+import {FindGame} from './pages/findGame/FindGame';
+import {Game} from './pages/game/Game';
+import {About} from './pages/about/About';
+import {Logout} from './pages/logout/Logout';
+import {UserStats} from './pages/userstats/UserStats';
+import {RequireAuthn} from './pages/AuthContainer';
+import {NotFound} from './pages/notFound/NotFound';
+import {Lobby} from './pages/lobby/Lobby';
+import {Rankings} from './pages/rankings/Rankings';
 
 export const webRoutes = {
     home: '/',
